96e86a22099f951dafe3b99fa3c05099f610c914
Cheeeeen3 <een0203@gmail.com>
2024-05-29 18:50:15 +0800

    add children to Login element

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { loader as InfoLoader } from "./components/Info.tsx"
 
-import './App.css'
-import Footer from './components/Footer.tsx'
-import Home from './components/Home.tsx'
-import Info from './components/Info.tsx'
-import { NavBar } from './components/NavBar.tsx'
-import React from 'react'
-import Login from './components/Login.tsx'
+import './App.css';
+import Footer from './components/Footer.tsx';
+import Home from './components/Home.tsx';
+import Info from './components/Info.tsx';
+import { NavBar } from './components/NavBar.tsx';
+import React from 'react';
+import Login from './components/Login.tsx';
+import SignUp from './components/SignUp.tsx';
 // FC type from React
 
 const route = createBrowserRouter([
@@ -27,7 +28,12 @@ const route = createBrowserRouter([
       {
         path: '/Login',
         element: <Login />,
-      }
+        // children: []
+      },
+      {
+        path: '/SignUp',
+        element: <SignUp />,
+      },
     ]
   }
 ])
