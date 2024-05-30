2d21656df75515046d7b4bfa790d025e24bd7e4e
Ray <gcjm0529@gmail.com>
2024-05-31 01:17:48 +0800

    add menu control

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const NavBar: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleClick = ()=>{
+        let menuBtn = document.getElementById('menu-btn')
+        menuBtn?.setAttribute('aria-expanded', (menuBtn.ariaExpanded !== 'true').toString());
+        setIsOpen(!isOpen);
+    };
+
     return (
         <header>
             <nav className="bg-gray-800">
@@ -9,25 +17,17 @@ const NavBar: React.FC = () => {
                     <div className="relative flex h-16 items-center justify-between">
                         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                             {/* <!-- Mobile menu button--> */}
-                            <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                            <button type="button" id='menu-btn' className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" onClick={handleClick} aria-controls="mobile-menu" aria-expanded="false">
                                 <span className="absolute -inset-0.5"></span>
                                 <span className="sr-only">Open main menu</span>
-                                {/* <!--
-                                Icon when menu is closed.
-
-                                Menu open: "hidden", Menu closed: "block"
-                                --> */}
-                                <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                {/* Icon when menu is closed. Menu open: "hidden", Menu closed: "block" */}
+                                {!isOpen && <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                                </svg>
-                                {/* <!--
-                                Icon when menu is open.
-
-                                Menu open: "block", Menu closed: "hidden"
-                                --> */}
-                                <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                </svg>}
+                                {/* <!--Icon when menu is open. Menu open: "block", Menu closed: "hidden"--> */}
+                                {isOpen && <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
+                                </svg>}
                             </button>
                         </div>
                         <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -65,17 +65,7 @@ const NavBar: React.FC = () => {
                                     </button>
                                 </div>
 
-                                {/* <!--
-                                Dropdown menu, show/hide based on menu state.
-
-                                Entering: "transition ease-out duration-100"
-                                From: "transform opacity-0 scale-95"
-                                To: "transform opacity-100 scale-100"
-                                Leaving: "transition ease-in duration-75"
-                                From: "transform opacity-100 scale-100"
-                                To: "transform opacity-0 scale-95"
-                                --> */}
-                                <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none hidden" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
+                                <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none animate-open-munu hidden" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
                                     {/* <!-- Active: "bg-gray-100", Not Active: "" --> */}
                                     <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Your Profile</a>
                                     <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-1">Settings</a>
@@ -87,7 +77,7 @@ const NavBar: React.FC = () => {
                 </div>
 
                 {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-                <div className="sm:hidden" id="mobile-menu">
+                {isOpen && <div className="sm:hidden" id="mobile-menu">
                     <div className="space-y-1 px-2 pb-3 pt-2">
                         {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
                         <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
@@ -95,7 +85,7 @@ const NavBar: React.FC = () => {
                         <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
                         <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
                     </div>
-                </div>
+                </div>}
             </nav>
         </header>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,22 @@ export default {
       colors: {
         'regal-blue': '#243c5a',
         'custom-green': '#16a34a',
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': {'transform': 'opacity: 0 scale(.95)'}},
+          '100%': {'transform': 'opacity: 1 scale(1)'}
+        },
+        'close-menu': {
+          '0%': {'transform': 'opacity: 1 scale(1)'},
+          '100%': {'transform': 'opacity: 0 scale(.95)'}
+        },
+      },
+      animation: {
+        'open-menu': 'open menu 100ms ease-out',
+        'close-menu': 'close menu 75ms ease-in',
       }
     },
-  },
   plugins: [],
 }
 
