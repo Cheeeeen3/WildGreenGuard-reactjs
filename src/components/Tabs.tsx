1697aa771c7cc0795d5759cb60a64a32fd8d48ec
Cheeeeen3 <een0203@gmail.com>
2024-06-13 22:39:44 +0800

    merged code from Tab.tsx into Tabs.tsx and import useTranslation from i18next for translation

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,30 +1,46 @@
 import React from "react";
-import Tab from './Tab.tsx'
-
-const tabNames = ['Invasive Plants', 'Native Plants']
-
-const active =
-  "inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500";
-const inactive =
-  "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300";
+import { useTranslation } from "react-i18next";
 
 interface props {
-  display: number,
-  handler: Function
+  display: number;
+  handler: Function;
 }
 
 const Tabs: React.FC<props> = ({ display, handler }) => {
+  const { t } = useTranslation();
 
+  const tabNames = [t("insp"), t("nasp")];
+
+  const active =
+    "inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500";
+  const inactive =
+    "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300";
 
   return (
     <>
       <section className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
         <div>
           <ul className="flex justify-center flex-wrap -mb-px">
-            {tabNames.map(tabname => 
-              tabNames.indexOf(tabname) + 1 === display ?
-              <Tab tabName={tabname} style={ active } handler={ ()=>handler(tabNames.indexOf(tabname) + 1) } key={tabname}/> :
-              <Tab tabName={tabname} style={ inactive } handler={ ()=>handler(tabNames.indexOf(tabname) + 1) } key={tabname}/>
+            {tabNames.map((tabname) =>
+              tabNames.indexOf(tabname) + 1 === display ? (
+                <li
+                  className="me-2"
+                  onClick={() => handler(tabNames.indexOf(tabname) + 1)}
+                >
+                  <a href="#" className={active} aria-current="page">
+                    {tabname}
+                  </a>
+                </li>
+              ) : (
+                <li
+                  className="me-2"
+                  onClick={() => handler(tabNames.indexOf(tabname) + 1)}
+                >
+                  <a href="#" className={inactive} aria-current="page">
+                    {tabname}
+                  </a>
+                </li>
+              )
             )}
           </ul>
         </div>
