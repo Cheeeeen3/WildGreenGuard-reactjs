bddf6b473c8d4afcfc89857f36cd0eb1fd61a286
Cheeeeen3 <een0203@gmail.com>
2024-06-27 17:12:13 +0800

    change file format of Validation

--- src/components/Validation.ts
+++ src/components/Validation.ts
@@ -1,23 +1,20 @@
+
 export interface props {
   email?: string;
   password?: string;
 }
-// export type ERRORS = {
-//     email?: string,
-//     password?: string
-// }
 
 const Validation = (values: props) => {
   const errors: props = {};
   const regex = /^[a-zA-Z0-9.]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/
   if (values.email === "") {
-    errors.email = "Email is required";
+    errors.email = "require email";
   } else if (!regex.test(values.email!)){
-    errors.email = "Email format is invalid";
+    errors.email = "invalid email";
   }
 
   if (values.password === "") {
-    errors.password = "Password is required";
+    errors.password = "require password";
   }
   return errors;
 };
