12badb1e9bc22f90ba64f6b9793fae7cf0a970e2
Cheeeeen3 <een0203@gmail.com>
2024-06-27 17:14:44 +0800

    change background color and font size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer: React.FC = () => {
   return (
     <>
-      <footer id="footer" className="bg-orange-100 text-xl font-medium bottom-0">
+      <footer id="footer" className="text-xs font-custom-font font-medium text-neutral-400 bottom-0">
         <div className="max-w-9xl mx-auto px-4 py-5 flex flex-row justify-end text-center sm:text-right sm:px-5 sm:gap-2">
           <p>Copyright &copy; <span id='year'>2024</span></p>
           <p>All Rights Reserved</p>
