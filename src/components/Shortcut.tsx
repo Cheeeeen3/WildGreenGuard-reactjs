03b8715d002ac567e14057dacda91b6a7a70ee68
Cheeeeen3 <een0203@gmail.com>
2024-07-14 20:37:00 +0800

    modify Shortcut style

--- src/components/Shortcut.tsx
+++ src/components/Shortcut.tsx
@@ -9,17 +9,16 @@ interface Props {
 
 const Shortcut: React.FC<Props> = ({ plant, imageUrl }) => {
   const { t } = useTranslation();
+
   return (
-    <li
-      className="w-1/4 mx-10 items-start py-4 px-2"
-    >
-      <a href={`#${plant}`} className="flex items-center">
+    <li className="group mx-auto w-full py-2 hover:text-emerald-600">
+      <a href={`#${plant}`} className="flex items-center px-12 sm:px-28">
         <Image
           imageUrl={imageUrl}
           plant={plant}
-          className="w-10 h-10 rounded-full border-2 border-gray-400"
+          className="size-12 rounded-full border-2 border-gray-400 group-hover:border-emerald-600"
         />
-        <p className="ml-4 hover:text-blue-600">{t(plant)}</p>
+        <p className="ml-4 whitespace-nowrap text-base">{t(plant)}</p>
       </a>
     </li>
   );
