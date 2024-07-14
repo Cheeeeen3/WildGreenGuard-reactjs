4271984ca4d675e9446ce3912e79a56d9e1aa4bf
Cheeeeen3 <een0203@gmail.com>
2024-07-14 20:15:27 +0800

    modify googleButtonStyle

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,8 @@ interface buttonProps {
 const Button: React.FC<buttonProps> = ({ type, isLink, children }) => {
   const primaryButtonStyle =
     "btn w-11/12 btn-active bg-emerald-800 border-emerald-800 text-white hover:bg-emerald-600 max-w-xs ml-2 sm:ml-10";
-  const googleButtonStyle = "btn w-11/12 btn-active bg-white max-w-xs ml-2 sm:ml-10";
+  const googleButtonStyle =
+    "btn w-11/12 btn-active bg-white max-w-xs ml-2 sm:ml-10 text-black dark:hover:text-white";
   const linkButtonStyle =
     "btn btn-active btn-link text-emerald-800 hover:text-emerald-600 dark:text-emerald-500 dark:hover:text-emerald-300";
 
