f8445628675610ce8cdf4d83d608ab2fdd9ac013
Cheeeeen3 <een0203@gmail.com>
2024-07-24 14:55:55 +0800

    modify the style of logo icon

--- src/components/LogoIcon.tsx
+++ src/components/LogoIcon.tsx
@@ -8,7 +8,7 @@ interface props {
 const LogoIcon: React.FC<props> = ({ width, height }) => {
   return (
     <img
-      className="mx-auto h-24 w-24"
+      className="mx-auto"
       src="/src/assets/HomePage_WGG/earth_nature_futaba.png"
       alt="Logo"
       width={width}
