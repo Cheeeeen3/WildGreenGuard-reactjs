0f4842d827bae5c01dab59944fc92e1ade9c488c
Cheeeeen3 <een0203@gmail.com>
2024-07-24 15:39:35 +0800

    modify style of web intro

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,10 +5,10 @@ const Home: React.FC = () => {
   const { t } = useTranslation();
   return (
     <>
-      <div className="flex flex-col sm:flex-row gap-4 place-items-center mx-auto px-16 py-16">
+      <div className="mx-auto flex flex-col place-items-center gap-4 px-16 py-16 sm:flex-row">
         <div>
           <img
-            className="flex flex-col rounded-2xl object-contain h-200 w-200"
+            className="h-200 w-200 flex flex-col rounded-2xl object-contain"
             src="https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg"
             alt="Orange-cat-yawn"
           />
@@ -28,7 +28,7 @@ const Home: React.FC = () => {
           />
         </div>
       </div>
-      <div className="text-center font-custom-font px-16 my-3 text-emerald-900 text-xl sm:text-2xl block">
+      <div className="my-3 block px-16 text-center font-custom-font text-xl sm:text-2xl">
         {t("webintro")}
       </div>
     </>
