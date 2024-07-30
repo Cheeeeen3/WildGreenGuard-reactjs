8bc1d63208ba2b723602811af5b351297f63d469
Cheeeeen3 <een0203@gmail.com>
2024-07-30 20:54:38 +0800

    update axios to get plantData and userInfo

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -2,19 +2,26 @@ import axios from "axios";
 import { UserState } from "./userSlice.tsx";
 
 // {"user":"111"
-  // "email":"een0203@gmail.com"}
-
+// "email":"een0203@gmail.com"}
 
 async function getUserInfo() {
-    try {
-      // const response = await axios.get("https://2885-42-73-188-144.ngrok-free.app/plants/info/data");
-      const response = await axios.get("host:port/admin/user/oauth2-login");
-      const { user, email } : UserState = response.data;
+  try {
+    // const response = await axios.get("https://2885-42-73-188-144.ngrok-free.app/plants/info/data");
+    const response = await axios.get("host:port/admin/user/oauth2-login");
+    if (response.status === 200) {
+      const { user, email }: UserState = response.data;
       return { user, email };
-    } catch (error) {
+    }
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error) && error.response?.status === 400) {
+      console.log(error.response.data.message);
+    } else if (error instanceof Error) {
+      console.log(error.message);
+    } else {
       console.error(error);
       return null;
     }
   }
-  
-  export default getUserInfo;
+}
+
+export default getUserInfo;
